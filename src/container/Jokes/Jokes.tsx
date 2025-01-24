dbbfb2b45737b01b410bdfa17998c97110f58713
Aidai Mashrapova <aidaimashrapova8@mail.ru>
2025-01-24 18:10:46 +0600

    Task 2 done. Joke fetching app created

--- src/container/Jokes/Jokes.tsx
+++ src/container/Jokes/Jokes.tsx
@@ -1,9 +1,25 @@
-
+import {useState, useEffect} from "react";
 
 const Jokes = () => {
-    return (
-        <div>
+    const [joke, setJoke] = useState<string>("");
 
+    useEffect(() => {
+        fetchJoke();
+    }, []);
+
+    const fetchJoke = async () => {
+            const response = await fetch("https://api.chucknorris.io/jokes/random");
+            const data = await response.json();
+            setJoke(data.value);
+    };
+
+    return (
+        <div className="container text-center">
+            <h2 className="m-4">Joke</h2>
+            <p className="bg-warning">{joke}</p>
+            <button className="btn btn-primary mt-3" onClick={fetchJoke}>
+                New Joke
+            </button>
         </div>
     );
 };
